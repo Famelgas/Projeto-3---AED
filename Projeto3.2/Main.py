00734df9b33a6d8f9743237aa457fd27cd524399
Famelgas <filipe.damendes@gmail.com>
2022-03-27 18:54:53 +0100

    comecei a fazer os testes

--- Projeto3.2/Main.py
+++ Projeto3.2/Main.py
@@ -1,4 +1,5 @@
 from sys import stdin, stdout
+import time
 
 class NFT:
     def __init__(self, name, hash_nft, value):
@@ -112,8 +113,8 @@ class SplayTree:
 
 
 
-def readln():
-    return stdin.readline().rstrip().split(" ")
+def readln(file):
+    return file.readline().rstrip().split(" ")
 
 
 def outln(string):
@@ -122,56 +123,92 @@ def outln(string):
 
 
 
-def main():
+def main(test_file):
     splay_tree = SplayTree()
-    try:
-        while True:
-            user_in = readln()
-            #start_time = time.time()
-            if user_in[0] == "ARTIGO":
-                if not splay_tree.find(user_in[1]):
-                    splay_tree.insert(user_in[1], user_in[2], user_in[3])
-                    outln("NOVO ARTIGO INSERIDO")
-                else:
-                    outln("ARTIGO JA EXISTENTE")
+    user_in = [""]
+    run_time = 0
 
-            elif user_in[0] == "OFERTA":
-                if not splay_tree.find(user_in[1]):
-                    outln("ARTIGO NAO REGISTADO")
-                else:
-                    splay_tree.insert_info(user_in[2])
-                    outln("OFERTA ATUALIZADA")
+    with open(test_file, "r") as file:
+        try:
+            while True:
+                user_in = readln(file)
+                start_time = time.time()
+                if user_in[0] == "ARTIGO":
+                    if not splay_tree.find(user_in[1]):
+                        splay_tree.insert(user_in[1], user_in[2], user_in[3])
+                        #outln("NOVO ARTIGO INSERIDO")
+                    else:
+                        pass
+                        #outln("ARTIGO JA EXISTENTE")
+
+                elif user_in[0] == "OFERTA":
+                    if not splay_tree.find(user_in[1]):
+                        pass
+                        #outln("ARTIGO NAO REGISTADO")
+                    else:
+                        splay_tree.insert_info(user_in[2])
+                        #outln("OFERTA ATUALIZADA")
 
 
 
-            elif user_in[0] == "CONSULTA":
-                if not splay_tree.find(user_in[1]):
-                    outln("ARTIGO NAO REGISTADO")
-                else:
-                    node = splay_tree.find(user_in[1])
-                    outln(node.nft.name + " " + node.nft.hash_nft + " " + str(node.nft.value))
-                    outln("FIM")
+                elif user_in[0] == "CONSULTA":
+                    if not splay_tree.find(user_in[1]):
+                        pass
+                        #outln("ARTIGO NAO REGISTADO")
+                    else:
+                        node = splay_tree.find(user_in[1])
+                        #outln(node.nft.name + " " + node.nft.hash_nft + " " + str(node.nft.value))
+                        #outln("FIM")
+                    
+
+                elif user_in[0] == "LISTAGEM":
+                    splay_tree.list_tree(splay_tree.root)
+                    #outln("FIM")
+
+                elif user_in[0] == "APAGA":
+                    splay_tree.delete_tree()
+                    #outln("CATALOGO APAGADO")
                 
-
-            elif user_in[0] == "LISTAGEM":
-                splay_tree.list_tree(splay_tree.root)
-                outln("FIM")
-
-            elif user_in[0] == "APAGA":
-                splay_tree.delete_tree()
-                outln("CATALOGO APAGADO")
-            
-            elif user_in[0] == "FIM":
-                break
-            
-            else:
-                break
-            #run_time += (time.time() - start_time)
-    except EOFError:
-        pass
-
+                elif user_in[0] == "FIM":
+                    break
+                
+                else:
+                    break
+                run_time += (time.time() - start_time)
+        except EOFError:
+            pass
+    return run_time
 
 
 
 if __name__=="__main__":
-    main()
+    num_cases = [100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000]
+    cenario_1_files = ["test_100000_cenario_1", "test_200000_cenario_1", "test_300000_cenario_1", "test_400000_cenario_1", "test_500000_cenario_1", "test_600000_cenario_1", "test_700000_cenario_1", "test_800000_cenario_1", "test_900000_cenario_1", "test_1000000_cenario_1"]
+    cenario_2_files = ["test_100000_cenario_2", "test_200000_cenario_2", "test_300000_cenario_2", "test_400000_cenario_2", "test_500000_cenario_2", "test_600000_cenario_2", "test_700000_cenario_2", "test_800000_cenario_2", "test_900000_cenario_2", "test_1000000_cenario_2"]
+
+
+    cenario_1_times = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    cenario_2_times = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    for i in range(len(cenario_1_files)):
+        run_time = main(cenario_1_files[i])
+        cenario_1_times[i] = run_time 
+         
+    for i in range(len(cenario_2_files)):
+        run_time = main(cenario_2_files[i])
+        cenario_2_times[i] = run_time
+
+
+    print("------------ Tempos Cenário 1 ------------")
+    for i in range(len(cenario_1_times)):
+        print("Tempo " + str(num_cases[i]) + " casos: ", end=" ")
+        print(cenario_1_times[i])
+
+    print("\n")
+
+    print("----------- Tempos Cenário 2 ------------")
+    for i in range(len(cenario_2_times)):
+        print("Tempo " + str(num_cases[i]) + " casos: ", end=" ")
+        print(cenario_2_times[i])
+
+
